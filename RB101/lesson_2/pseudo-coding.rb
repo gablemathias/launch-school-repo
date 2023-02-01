47f815684ac236d7def8c68c93cbee2f5c337203
Gabriel Mathias <gablemathias@icloud.com>
2023-02-01 17:07:37 -0300

    pseudo-coding practice

---
+++ RB101/lesson_2/pseudo-coding.rb
@@ -0,0 +1,40 @@
+# 1. returns the sum of two integers
+# get the first number and store it
+# get the second number and store it
+# perform the addition using the first number and the second number and store it
+# return the resulting valu
+
+# 2. A method that takes an array of strings, and returns a string that is
+# all those strings concatenated together
+# get an array of strings
+# set an empty variable
+# while looping through each value of the array store the value inside the empty variable
+# concatenating with the previous value
+# return that variable
+
+# 3. a method that takes an array of integers, and returns a new array with every other
+# element from the original array, starting with the first element. For instance:
+# get an array of integers
+# set an empty array
+# loop through the array
+# check if its not the last item of the array to stop
+# store one value in it and discard the next value
+# return the filled array
+
+# 4. a method that determines the index of the 3rd occurrence of a given character in a string.
+# get a string
+# set the given character of that string
+# set an empty variable
+# loop through the string
+# sum the number of occurrencies of that given character and store it
+# if the character dont occur as least 3 times returns null
+# when reaching the 3rd occurrency get the index of the last occurency
+# stop the loop  and returns it
+
+# 5. a method that takes two arrays of numbers and returns the result of merging the arrays.
+# get the first array
+# get the second array
+# set a new empty array
+# loop the length of array
+# store the value of the first array followed the value of the second array
+# return the new array

2820a46e3ce0abf63fe26eadabec644cf680e9ff
Gabriel Mathias <gablemathias@icloud.com>
2023-02-02 16:13:32 -0300

    add looping mechanism and integer validation

--- RB101/lesson_2/calculator.rb
+++ RB101/lesson_2/calculator.rb
@@ -3,31 +3,62 @@
 # perform the operation on the two numbers
 # output the result
 
+number1 = ''
+number2 = ''
+
 def prompt(message)
   Kernel.puts("=> #{message}")
 end
 
-prompt("Welcome to calculator!")
-
-prompt("What's the first number?")
-number1 = Kernel.gets().chomp().to_i
-
-prompt("What's the second number?")
-number2 = Kernel.gets().chomp().to_i
-
-prompt("What operation would you like to perform? 1) add 2) subtract 3) multiply 4) divide")
-operator = Kernel.gets().chomp()
-
-if operator == '1'
-  result = number1 + number2
-elsif operator == '2'
-  result = number1 - number2
-elsif operator == '3'
-  result = number1 * number2
-elsif operator == '4'
-  result = number1.to_f() / number2
-else
-  prompt("Invalid operator")
+def valid_number?(num)
+  num.to_i() != 0
 end
 
-prompt("The result is #{result}")
+prompt("Welcome to calculator!")
+
+loop do
+  loop do
+    prompt("What's the first number?")
+    number1 = Kernel.gets().chomp()
+
+    if valid_number?(number1)
+      break
+    else
+      prompt("Hmmm.. that doesn't look like a valid number")
+    end
+  end
+
+  loop do
+    prompt("What's the second number?")
+    number2 = Kernel.gets().chomp()
+
+    if valid_number?(number2)
+      break
+    else
+      prompt("Hmmm.. that doesn't look like a valid number")
+    end
+  end
+
+  prompt("What operation would you like to perform? 1) add 2) subtract 3) multiply 4) divide")
+  operator = Kernel.gets().chomp()
+  result = case operator
+           when '1'
+            number1.to_i + number2.to_i()
+           when '2'
+            number1.to_i - number2.to_i()
+           when '3'
+            number1.to_i * number2.to_i()
+           when '4'
+            number1.to_f() / number2.to_i()
+           else
+            prompt("Invalid operator")
+           end
+
+  prompt("The result is #{result}")
+
+  prompt("Do you want to perform another calculation? (Y to calculate again)")
+  answer = Kernel.gets().chomp()
+  break unless answer.downcase().start_with?('y')
+end
+
+prompt("Thank you for using the calculator! See ya")

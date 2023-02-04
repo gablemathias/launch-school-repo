14cbeb50aa8d9c8de9b224a5853c6868478b8f7b
Gabriel Mathias <gablemathias@icloud.com>
2023-02-04 15:46:12 -0300

    mortgage calculator

---
+++ RB101/lesson_2/mortgage_calculator/mortgage_calculator.rb
@@ -0,0 +1,33 @@
+def get_float_value
+  gets.chomp.to_f
+end
+
+def get_int_value
+  gets.chomp.to_i
+end
+
+def convert_apr(apr)
+  apr / 12 / 100
+end
+
+def convert_loan_duration(loan)
+  (loan * 12).to_i
+end
+
+def prompt(message)
+  puts("=> #{message}")
+end
+
+def monthly_payment(l_amount, m_interest, loan_duration)
+  mp = l_amount * (m_interest / (1 - (1 + m_interest)**(-loan_duration)))
+  mp.truncate(2)
+end
+
+prompt("Hello, what's the loan amount?")
+loan_amount = get_float_value
+prompt("What's the Annual Percentage Rate?")
+monthly_interest_rate = convert_apr(get_float_value)
+prompt("What's the monthly loan duration?")
+monthly_loan_duration = get_int_value
+
+puts monthly_payment(loan_amount, monthly_interest_rate, monthly_loan_duration)

60615f020c1a3acb4d1623595506721bbbea991e
Gabriel Mathias <gablemathias@icloud.com>
2023-02-06 16:25:56 -0300

    refactor play_again and valid_choice to methods)

--- RB101/lesson_2/rock_paper_scissors.rb
+++ RB101/lesson_2/rock_paper_scissors.rb
@@ -4,36 +4,44 @@ def prompt(message)
   Kernel.puts("=> #{message}")
 end
 
+def display_result(player, computer)
+  if (player == 'rock' && computer == 'scissors') ||
+    (player == 'paper' && computer == 'rock') ||
+    (player == 'scissors' && computer == 'paper')
+   prompt("You won!")
+ elsif (player == computer)
+   prompt("A tie!")
+ else
+   prompt("Computer won!")
+ end
+end
+
+def valid?(choice)
+  VALID_CHOICES.include?(choice)
+end
+
+def play_again
+  prompt("Do you want to play again?")
+  answer = Kernel.gets().chomp().downcase
+  answer.start_with?('y')
+end
+
 loop do
   choice = ''
   loop do
     prompt("Choose one: #{VALID_CHOICES.join(', ')}")
     choice = Kernel.gets().chomp().downcase()
 
-    if VALID_CHOICES.include?(choice)
-      break
-    else
-      prompt("That's not a valid choice.")
-    end
+    break if valid?(choice)
+    prompt("That's not a valid choice.")
   end
 
   computer_choice = VALID_CHOICES.sample
+
+  display_result(choice, computer_choice)
   prompt("You choose: #{choice}; Computer choose: #{computer_choice}")
 
-  if (choice == 'rock' && computer_choice == 'scissors') ||
-     (choice == 'paper' && computer_choice == 'rock') ||
-     (choice == 'scissors' && computer_choice == 'paper')
-    prompt("You won!")
-  elsif (choice == computer_choice)
-    prompt("A tie!")
-  else
-    prompt("Computer won!")
-  end
-
-  prompt("Do you want to play again?")
-  answer = Kernel.gets().chomp().downcase
-
-  break unless answer.start_with?('y')
+  break unless play_again()
 end
 
 prompt("Thank you for playing!")

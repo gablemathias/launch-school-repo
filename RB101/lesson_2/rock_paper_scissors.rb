7d0b4c22a2fbf90511398b5a1d4d2a7af9e5ec46
Gabriel Mathias <gablemathias@icloud.com>
2023-02-06 08:03:02 -0300

    add validation choices

--- RB101/lesson_2/rock_paper_scissors.rb
+++ RB101/lesson_2/rock_paper_scissors.rb
@@ -18,3 +18,13 @@ end
 
 computer_choice = VALID_CHOICES.sample
 prompt("You choose: #{choice}; Computer choose: #{computer_choice}")
+
+if (choice == 'rock' && computer_choice == 'scissors') ||
+   (choice == 'paper' && computer_choice == 'rock') ||
+   (choice == 'scissors' && computer_choice == 'paper')
+  prompt("You win!")
+elsif (choice == computer_choice)
+  prompt("A tie!")
+else
+  prompt("Computer win!")
+end

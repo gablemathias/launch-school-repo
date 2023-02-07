8c5b23296919a188835c4fec615a3b47b2a1a316
Gabriel Mathias <gablemathias@icloud.com>
2023-02-07 16:17:46 -0300

    refactoring game to add score and looping

--- RB101/lesson_2/rock_paper_scissors/rock_paper_scissors.rb
+++ RB101/lesson_2/rock_paper_scissors/rock_paper_scissors.rb
@@ -9,13 +9,11 @@ def prompt(message)
 end
 
 def win?(first, second)
-  if WIN_CHOICES.include?([first, second])
-    "You won!"
-  elsif WIN_CHOICES.include?([second, first])
-    "Computer won!"
-  else
-    "It's a tie!"
-  end
+  WIN_CHOICES.include?([first, second])
+end
+
+def lose?(first, second)
+  WIN_CHOICES.include?([second, first])
 end
 
 def valid?(choice)
@@ -39,22 +37,56 @@ def retrieve(choice)
   end
 end
 
-loop do
-  choice = ''
-  loop do
-    prompt("Choose one: #{VALID_CHOICES.join(', ')}")
-    choice = retrieve(Kernel.gets().chomp().downcase())
-
-    break if valid?(choice)
-    prompt("That's not a valid choice.")
+def end_game?(player_score, computer_score)
+  if player_score == 3
+    prompt("Player is the Winner!")
+    true
+  elsif computer_score == 3
+    prompt("Computer is the Winner!")
+    true
+  else
+    false
   end
+end
 
-  computer_choice = VALID_CHOICES.sample
+def game
+  player_score = 0
+  computer_score = 0
+  loop do
+    choice = ''
 
-  puts win?(choice, computer_choice)
-  prompt("You choose: #{choice}; Computer choose: #{computer_choice}")
+    loop do
+      prompt("Choose one: #{VALID_CHOICES.join(', ')}")
+      choice = retrieve(Kernel.gets().chomp().downcase())
 
-  break unless play_again?()
+      break if valid?(choice)
+      prompt("That's not a valid choice.")
+    end
+
+    computer_choice = VALID_CHOICES.sample
+
+    prompt("You choose: #{choice}; Computer choose: #{computer_choice}")
+
+    if win?(choice, computer_choice)
+      player_score += 1
+      prompt("You won this round!")
+    elsif lose?(choice, computer_choice)
+      computer_score += 1
+      prompt("Computer won this round!")
+    else
+      prompt("It's a Tie!")
+    end
+
+    prompt("Score: \n Player: #{player_score} \n Computer: #{computer_score}")
+
+    break if end_game?(player_score, computer_score)
+  end
+end
+
+game()
+
+while play_again?() do
+  game()
 end
 
 prompt("Thank you for playing!")

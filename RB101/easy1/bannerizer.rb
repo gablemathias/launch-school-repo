f0f724cc53dca0c59d59e01d29abde8e7d935c46
Gabriel Mathias <gablemathias@icloud.com>
2023-02-14 10:51:49 -0300

    bannerizer

--- RB101/easy1/bannerizer.rb
+++ RB101/easy1/bannerizer.rb
@@ -1,1 +1,19 @@
 # Write a method that will take a short line of text, and print it within a box.
+
+def print_in_box(str)
+  box_size = str.size + 2
+  horizontal_length = "-" * box_size
+  vertical_length = " " * box_size
+  horizontal_line = "+#{horizontal_length}+"
+  vertical_line = "|#{vertical_length}|"
+
+  puts(horizontal_line)
+  puts(vertical_line)
+  puts("| #{str} |")
+  puts(vertical_line)
+  puts(horizontal_line)
+end
+
+print_in_box('')
+
+print_in_box('Something in the rain')

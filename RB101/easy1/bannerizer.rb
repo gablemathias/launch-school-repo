7ad37c3f82ddb74e01743fff9071dc07336fcc50
Gabriel Mathias <gablemathias@icloud.com>
2023-02-14 07:36:38 -0300

    easy1)

---
+++ RB101/easy1/bannerizer.rb
@@ -0,0 +1,1 @@
+# Write a method that will take a short line of text, and print it within a box.
--- RB101/easy1/whats_my_bonuses.rb
+++ RB101/easy1/whats_my_bonuses.rb
@@ -4,7 +4,7 @@
 # else the bonus is 0.
 
 def calculate_bonus(salary, has_bonus)
-  has_bonus ? salary / 2 : 0
+  has_bonus ? (salary / 2) : 0
 end
 
 puts calculate_bonus(2800, true) == 1400

e2cc19bfddb04fb5dae9af44f7f54b6a986dc8a3
Gabriel Mathias <gablemathias@icloud.com>
2023-02-13 23:37:01 -0300

    add comment on precedence

--- RB101/lesson_2/precedence.rb
+++ RB101/lesson_2/precedence.rb
@@ -4,3 +4,7 @@ def value(num)
 end
 
 puts value(3) + value(5) * value(7)
+
+# In an arithmetic expression ruby first evaluates everything from left-to-right
+# before calling any operator. After that it evaluates its operands
+# using the precedence order.
